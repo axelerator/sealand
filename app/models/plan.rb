67afa0e154f6f8bc47050d22800863e020e463cb
Klas Kalass <klas.kalass@freiheit.com>
2012-06-02 19:17:52 +0200

    of course we need to allow access to the plan_image

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,5 +1,5 @@
 class Plan < ActiveRecord::Base
-  attr_accessible :description, :name, :user_id
+  attr_accessible :description, :name, :user_id, :plan_image
   belongs_to :user
   has_file :plan_image, PlanImageUploader
   has_many :workshops
