6da6e8dec3be6e35da34d421f856afee382175f6
Jan Ahrens <jan.ahrens+git@googlemail.com>
2012-06-02 17:27:31 +0200

    Added basic API to create materials

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,3 +1,5 @@
 class Material < ActiveRecord::Base
-  attr_accessible :name
+  validates_presence_of :name
+
+  attr_accessible :name, :description, :langitude, :longitude, :accuracy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120602141403) do
+ActiveRecord::Schema.define(:version => 20120602151704) do
 
   create_table "materials", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                  :null => false
+    t.datetime "updated_at",                  :null => false
+    t.string   "description", :default => ""
+    t.float    "langitude"
+    t.float    "longitude"
+    t.float    "accuracy"
   end
 
   create_table "plans", :force => true do |t|
