8c349431ba10e281bba30b9c46246893941a2f4b
Marcus Thiesen <marcus.thiesen@freiheit.com>
2012-06-02 19:02:37 +0200

    Rename Latitude field to correct name

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,9 +1,9 @@
 class Material < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :description
-  validates_numericality_of :langitude
+  validates_numericality_of :latitude
   validates_numericality_of :longitude
   validates_numericality_of :accuracy
 
-  attr_accessible :name, :description, :langitude, :longitude, :accuracy
+  attr_accessible :name, :description, :latitude, :longitude, :accuracy
 end
