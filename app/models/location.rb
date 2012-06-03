6c98061c4f7127b124ea1b8394bbbc36a7a2e413
Marcus Thiesen <marcus.thiesen@freiheit.com>
2012-06-03 12:42:50 +0200

    JSON API Fixing

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,6 @@
 class Location < ActiveRecord::Base
   belongs_to :material
 
-  validates_presence_of :description
   validates_numericality_of :latitude
   validates_numericality_of :longitude
   validates_numericality_of :accuracy
